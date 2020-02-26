0c365ec7bc01b71c5d2e77d90a7ab75c7920930b
JuxhinBleta <juxhinbleta@gmail.com>
2020-02-26 23:53:49 +0100

    display contact numbers

--- src/module/contacts/component/Contact.tsx
+++ src/module/contacts/component/Contact.tsx
@@ -12,14 +12,30 @@ function Contact(
   }: ContactProps,
 ) {
   return (
-    <div className="flex justify-between px-2 py-2 hover:bg-blue-100 rounded cursor-pointer">
-      <p className="flex text-gray-700">
-        <svg className="h2 w-2 text-gray-500 mx-2" viewBox="0 0 8 8" fill="currentColor">
-          <circle cx="4" cy="4" r="3"/>
-        </svg>
-        {`${firstName} ${lastName}`}
-      </p>
-      <p className="text-gray-500 font-thin">{rel}</p>
+    <div className={"flex flex-col hover:bg-blue-100 mb-1"}>
+      <div className="flex justify-between px-2 py-2  rounded cursor-pointer">
+        <p className="flex text-gray-700 font-bold">
+          <svg className="h2 w-2 text-gray-500 mx-2" viewBox="0 0 8 8" fill="currentColor">
+            <circle cx="4" cy="4" r="3"/>
+          </svg>
+          {`${firstName} ${lastName}`}
+        </p>
+        <p className="text-gray-500 font-thin">{rel}</p>
+      </div>
+      {
+        numbers && numbers.length && numbers.map((number, index)=>{
+          return(
+            <div className="flex justify-between px-2 py-2  rounded cursor-pointer">
+              <p className="flex text-gray-600 capitalize text-sm">
+                <svg className="h1 w-1 text-gray-500 mx-2" viewBox="0 0 8 8" fill="currentColor">
+                  <circle cx="4" cy="4" r="3"/>
+                </svg>
+                {`${number.type} ${number.number}`}
+              </p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
